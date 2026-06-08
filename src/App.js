7f8861848c8cbf1952ac0e99095195d0b0969cc2
soukainahalimi01 <soukainahalimi01@gmail.com>
2026-06-08 14:50:27 +0100

    cont

--- src/App.js
+++ src/App.js
@@ -1,17 +1,30 @@
 import './App.css';
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 
 import Signup from './auth/Signup';
 import Login from './auth/Login';
 import Contact from './auth/Contact';
+import ProtectedRoute from './auth/ProtectedRoute';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Contact />} />
+
+        <Route path="/" element={<Navigate to="/contact" />} />
+
         <Route path="/inscription" element={<Signup />} />
         <Route path="/connexion" element={<Login />} />
+
+        <Route
+          path="/contact"
+          element={
+            <ProtectedRoute>
+              <Contact />
+            </ProtectedRoute>
+          }
+        />
+
       </Routes>
     </BrowserRouter>
   );
