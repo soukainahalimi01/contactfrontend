0f42879747c5161de19bd45ad3ecc4b50da0820c
soukainahalimi01 <soukainahalimi01@gmail.com>
2026-06-09 17:47:03 +0100

    re

--- src/auth/Contact.jsx
+++ src/auth/Contact.jsx
@@ -1,9 +1,72 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
 
 function Contact() {
+  const [contacts, setContacts] = useState([]);
+
+  useEffect(() => {
+    loadContacts();
+  }, []);
+
+  const loadContacts = async () => {
+    try {
+      const token = localStorage.getItem("token");
+
+
+      const response = await fetch("http://localhost:8080/api/contacts", {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${token}`,
+        },
+      });
+
+      if (!response.ok) {
+        throw new Error("Erreur de chargement");
+      }
+
+      const data = await response.json();
+
+      setContacts(data);
+    } catch (error) {
+      console.error("ERREUR =", error);
+    }
+  };
+
   return (
-    <div>Contact</div>
-  )
+    <div>
+      <h2>Mes Contacts</h2>
+
+      <table border="1">
+        <thead>
+          <tr>
+            <th>Nom</th>
+            <th>Prénom</th>
+            <th>Téléphone</th>
+            <th>Email</th>
+            <th>Ville</th>
+          </tr>
+        </thead>
+
+        <tbody>
+          {contacts.length > 0 ? (
+            contacts.map((contact) => (
+              <tr key={contact.id}>
+                <td>{contact.lastName}</td>
+                <td>{contact.firstName}</td>
+                <td>{contact.tel}</td>
+                <td>{contact.email}</td>
+                <td>{contact.ville}</td>
+              </tr>
+            ))
+          ) : (
+            <tr>
+              <td colSpan="5">Aucun contact trouvé</td>
+            </tr>
+          )}
+        </tbody>
+      </table>
+    </div>
+  );
 }
 
-export default Contact
+export default Contact;
--- src/auth/ProtectedRoute.jsx
+++ src/auth/ProtectedRoute.jsx
@@ -1,17 +1,13 @@
 import { Navigate } from "react-router-dom";
 function ProtectedRoute({ children }) {
-  console.log("ProtectedRoute RENDERED"); // 👈 زيد هاد
 
   const token = localStorage.getItem("token");
 
-  console.log("TOKEN:", token);
 
   if (!token) {
-    console.log("NO TOKEN -> redirect");
     return <Navigate to="/connexion" replace />;
   }
 
-  console.log("TOKEN OK -> allow access");
   return children;
 }
 export default ProtectedRoute;
